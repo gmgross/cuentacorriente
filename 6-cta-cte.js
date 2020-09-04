9a1fc517f91a4b56dc5a6d0e382a686f9c2b939f
GERMAN MARTINEZ <germanmartinezgross@outlook.com>
2020-09-04 11:27:27 -0300

    commit

--- 6-cta-cte.js
+++ 6-cta-cte.js
@@ -1,85 +1,62 @@
+const $monto = document.getElementById("monto");
+const $depositar = document.getElementById("depositar");
+const $extraer = document.getElementById("extraer");
+const $datos = document.getElementById("datos");
+const $balance = document.getElementById("balance");
+let balance = 0;
 
-function agregarOperacion(tipoOperacion, montoIngresado){
-    if (document.getElementById(montoIngresado).value != 0) {
-        let balance = calculoBalance(tipoOperacion, document.getElementById(montoIngresado).value);
-        if (balance != -1) {
-            agregarLineaTabla(tipoOperacion, document.getElementById(montoIngresado).value, balance);
-            document.getElementById("CuentaCorriente").style.display = '';
-            cambioBoton("accionado","inicial","balanceTotal")
-        }        
-    }
-    
+function depositar() {
+  let monto = $monto.value;
+  monto = parseInt(monto);
+  if (revisionMonto() == false) {
+    return;
+  }
+  balance = balance + monto;
+  operacion(monto);
+};
+function revisionMonto(){
+    let monto = $monto.value;
+    monto = parseInt(monto);
+    if (monto < 0 || isNaN(monto)) {
+        alert("Ingrese un monto");
+        borrarMonto();
+        return false;
+      }
 }
 
-function agregarLineaTabla(tipoOperacion, montoIngresado, balance) {
-    let tbody = document.getElementById("bodyMovimientos"); 
-    let hilera = document.createElement("tr");
-    let textoCelda
-    for (var j = 0; j < 3; j++) {
-        let celda = document.createElement("td");
-      switch (j) {
-        case 0:
-            textoCelda = document.createTextNode(DateAString(new Date()));
-            break;
-        case 1:
-              if (tipoOperacion == 'extraccion') {
-                celda.classList.add("text-danger");
-                montoIngresado = montoIngresado * -1;
-              } else {
-                celda.classList.add("text-success");
-              }
-              textoCelda = document.createTextNode("$"+montoIngresado);
-              break;
-        case 2:
-              textoCelda = document.createTextNode(balance);
-              break;
-      
-          default:
-              break;
-      }      
-      celda.append(textoCelda);
-      hilera.append(celda);
-    }
-    tbody.append(hilera);
+function extraer() {
+  let monto = $monto.value;
+  monto = parseInt(monto);
+  if (revisionMonto() == false) {
+    return;
+  }
+  if (balance - monto < 0) {
+    alert("Saldo insuficiente");
+    borrarMonto();
+    return;
+  }
+  balance = balance - monto;
+  operacion(monto * -1);
+};
+
+function operacion(monto) {
+  const clase = monto <= 0 ? "text-danger" : "text-success";
+  const fecha = new Date();
+  const dia = fecha.getDate();
+  const mes = fecha.getMonth();
+  const anio = fecha.getFullYear();
+  const tr = document.createElement("tr");
+  const template = `
+                  <td>${dia}/${mes}/${anio}</td>
+                  <td class="${clase} font-weight-bold">$${monto}</td>
+                  <td>$${balance}</td>
+              `;
+  tr.innerHTML = template;
+  $datos.appendChild(tr);
+  $balance.innerHTML = `$${balance}`;
+  borrarMonto();
 }
 
-function cambioBoton(formulario, bloqueInicial,balanceTotal){
-    var blokinicial = document.getElementById(bloqueInicial);
-    if (document.getElementById(bloqueInicial).style.display == '' ) {
-        document.getElementById(formulario).style.display = '';
-        document.getElementById(balanceTotal).style.display = '';
-        document.getElementById(bloqueInicial).style.display ='none';
-
-    } else {
-        document.getElementById(formulario).style.display = 'none';
-        document.getElementById(bloqueInicial).style.display ='';    
-    }
+function borrarMonto() {
+  $monto.value = "";
 }
-
-
-
-function calculoBalance( tipoOperacion, montoIngresado){
-    let balanceActual = document.querySelector("span.balance").innerHTML.slice(1);
-    balanceActual = parseInt(balanceActual,10);
-    if (tipoOperacion == 'extraccion') {
-        balanceActual = ( balanceActual - montoIngresado );
-        if (balanceActual < 0) {
-            alert("No posee suficiente dinero, trate depositando");
-            balanceActual = -1;
-        }
-    } else {
-        balanceActual = Number(montoIngresado) + Number(balanceActual);        
-    }
-    if (balanceActual != -1) {
-        balanceActual = "$"+balanceActual;
-        document.querySelector("span.balance").innerHTML = balanceActual; 
-    }    
-    return balanceActual;
-}
-
-function DateAString(date){
-    let dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
-    let MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
-    let yyyy = date.getFullYear();
-    return (dd + "/" + MM + "/" + yyyy);
- }
